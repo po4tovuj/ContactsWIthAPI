80ef866ca451cb92bce47513673fe4700ae9ab7a
Sergey Miniaylo <po4tovuj@gmail.com>
2023-03-14 18:47:07 +0200

    add swagger file

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const cors = require('cors');
 const contactsRouter = require('./routes/api/contacts');
 const authRouter = require('./routes/api/auth');
 const usersRouter = require('./routes/api/users');
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 require('dotenv').config();
 const app = express();
 
@@ -14,7 +15,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/contacts', contactsRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
